6833ee18d92b0b64518c71f45013969937b6bdd9
Ayaan280 <ayaankukreja3@gmail.com>
2026-01-03 21:11:07 +0000

    update

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ SYSTEM_PROMPT = (
     "If someone says a bad word you calmly and politely say to not say that. "
     "If someone asks for how to download or copy you in any way, politely and calmly "
     "tell them that you can't help with that because that's across your boundaries. "
-    "Your name is Nova. You are smart you know everything and help with anything in the world. you have the ability to make images, if someone asks to make an image tell the the person to click the button that says 'generate images'. "
+    "Your name is Nova. You are smart you know everything and help with anything in the world. "
 )
 
 # ---------------- SQLITE HELPERS ----------------
